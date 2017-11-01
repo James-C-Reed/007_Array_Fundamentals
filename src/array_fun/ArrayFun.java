7a1e30b058d80f57392ad74332b80188d1d72779
compsci <compsci@compsci>
2017-11-01 10:01:53 -0600

    Innital Commit

---
+++ src/array_fun/ArrayFun.java
@@ -0,0 +1,16 @@
+package array_fun;
+
+import javax.swing.JOptionPane;
+
+public class ArrayFun {
+	
+	public void showInstructions() {
+		
+		String msg = "Essential rules for arrays";
+		String msg1 = "1. Arrays have a FIXED length!!! Ath the time they are created.";
+		
+		JOptionPane.showMessageDialog(null, msg);
+		
+	}
+
+}
---
+++ src/array_fun/TheMainClass.java
@@ -0,0 +1,11 @@
+package array_fun;
+
+public class TheMainClass {
+	
+public static void main(String[] args) {
+		
+		ArrayFun learnArrays = new ArrayFun();
+		
+	}
+
+}
