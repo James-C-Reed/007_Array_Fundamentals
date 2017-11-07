5f87145b6e7b4f298901d4de6a22fa7941dd80e9
compsci <compsci@compsci>
2017-11-07 10:20:21 -0700

    matrix update

--- src/array_fun/Matrix.java
+++ src/array_fun/Matrix.java
@@ -3,18 +3,26 @@ package array_fun;
 public class Matrix {
 	
 	private int[][] ticTacToe= new int [3][3];
-	
+	{
 	ticTacToe[0][0] = 0;
-	ticTacToe[0][1] = 0;
-	ticTacToe[0][2] = 0;
+	ticTacToe[0][1] = 1;
+	ticTacToe[0][2] = 2;
 	
-	ticTacToe[1][0] = 0;
-	ticTacToe[1][1] = 0;
-	ticTacToe[1][2] = 0;
+	ticTacToe[1][0] = 10;
+	ticTacToe[1][1] = 11;
+	ticTacToe[1][2] = 12;
 	
-	ticTacToe[2][0] = 0;
-	ticTacToe[2][1] = 0;
-	ticTacToe[2][2] = 0;
+	ticTacToe[2][0] = 20;
+	ticTacToe[2][1] = 21;
+	ticTacToe[2][2] = 22;
+	
+};
+
+	for (int r = 0; r < ticTacToe.length; r++) {
+	
+		System.out.println("row = " + r);
+		
+	}
 	
 public void build_2D_array() {
 	
