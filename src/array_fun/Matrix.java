f7167c9d0cca902c0f587e2bc75d227b6a0aa7b2
compsci <compsci@compsci>
2017-11-09 10:09:21 -0700

    init tests done for grade allow players to place X's and 0's(no overplacement) and have end

--- src/array_fun/Matrix.java
+++ src/array_fun/Matrix.java
@@ -2,32 +2,97 @@ package array_fun;
 
 public class Matrix {
 	
-	private int[][] ticTacToe= new int [3][3];
-	{
-	ticTacToe[0][0] = 0;
-	ticTacToe[0][1] = 1;
-	ticTacToe[0][2] = 2;
+	private int[][] ticTacToe = {
+			{00,01,02},
+			{10,11,12},
+			{20,21,22}
+	};
 	
-	ticTacToe[1][0] = 10;
-	ticTacToe[1][1] = 11;
-	ticTacToe[1][2] = 12;
+	private int[][] game = {
+			{0,0,0},
+			{0,1,0},
+			{0,0,0}
+	};
 	
-	ticTacToe[2][0] = 20;
-	ticTacToe[2][1] = 21;
-	ticTacToe[2][2] = 22;
+	private char[][] realGame = {
+			{'-','-','-'},
+			{'-','-','-'},
+			{'-','-','-'}
+	};
 	
-};
+	String[][] checkers = new String[8][8];
+	
+	public void test2D() {
+		
+		checkers[0][0] = "red";
+		checkers[0][7] = "red king";
+		checkers[3][4] = " ";
+		checkers[7][0] = "black";
+		checkers[7][7] = "black king";
+		
+		System.out.println("\nTest of 2D array\n");
+		
+		for (int row = 0; row <= 2; row++) {
+			for (int col = 0; col <= 2; col++) {
+				System.out.println("test ticTacToe[" + row + "][" + col + "] = " + ticTacToe[row][col]);
+			}
+		}
+		
+		System.out.println("\nprint 3x3\n");
+				
+		for (int row = 0; row < ticTacToe.length ; row++) {
+			for (int col = 0; col < ticTacToe[0].length; col++) {
+				System.out.print(ticTacToe[row][col] + " ");	
+			}
+		System.out.println();
+		}
+		
+	}
 
-	for (int r = 0; r < ticTacToe.length; r++) {
-	
-		System.out.println("row = " + r);
+	public void gameboard() {
+		
+		System.out.println("\nprint initial gameboard\n");
+		
+		for (int row = 0; row < game.length ; row++) {
+			for (int col = 0; col < game[0].length; col++) {
+				System.out.print(game[row][col] + " ");	
+			}
+		System.out.println();
+		}
+		
+		System.out.println("\now set row 0 col 0 to 1\n");
+		game[0][0] = 1;
+		for (int row = 0; row < game.length ; row++) {
+			for (int col = 0; col < game[0].length; col++) {
+				System.out.print(game[row][col] + " ");	
+			}
+		System.out.println();
+		}
+		
+		System.out.println("\nfinaly set row 2 col 2 to 1\n");
+		game[2][2] = 1;
+		for (int row = 0; row < game.length ; row++) {
+			for (int col = 0; col < game[0].length; col++) {
+				System.out.print(game[row][col] + " ");	
+			}
+		System.out.println();
+		}
+		
+		
+		System.out.println("\nfinal test\n");
+		realGame[0][1] = 'X';
+		realGame[1][1] = 'X';
+		realGame[2][2] = '0';
+		for (int row = 0; row < realGame.length ; row++) {
+			for (int col = 0; col < realGame[0].length; col++) {
+				System.out.print(realGame[row][col] + " ");	
+			}
+		System.out.println();
+		}
+		
+		
 		
 	}
 	
-public void build_2D_array() {
 	
-	System.out.println("...building a 2D array, meaning rows and collums");
-	
-}
-
 }
--- src/array_fun/TheMainClass.java
+++ src/array_fun/TheMainClass.java
@@ -4,14 +4,16 @@ public class TheMainClass {
 	
 	public static void main(String[] args) {
 		
-		confirmMain("Weare in main()");
+		confirmMain("We are in main()");
 		
 		ArrayFun learnArrays = new ArrayFun();
-		learnArrays.showInstructions();
-		learnArrays.intArrayExample();
+		//learnArrays.showInstructions();
+		//learnArrays.intArrayExample();
 		
 		Matrix my2D = new Matrix();
-		my2D 
+		//my2D.build_2D_array();
+		//my2D.test2D();
+		my2D.gameboard();
 		
 	}
 
